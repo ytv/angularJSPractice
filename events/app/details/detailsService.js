ddb098181c88777059d510dae44a1bfd83224c87
ytv <sillying@gmail.com>
2016-07-24 10:01:13 -0700

    Merged detailsService into eventsService

--- events/app/details/detailsService.js
+++
@@ -1,9 +0,0 @@
-app.service('detailsService', ['$q',
-function($q) {
-    this.rsvp = function(id, response) {
-        return $q(function(resolve, reject) {
-            // TODO: Implement request to update RSVP for event
-            resolve(true);
-        });
-    }
-}]);
